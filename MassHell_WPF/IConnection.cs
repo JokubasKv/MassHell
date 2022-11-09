c506ba9df363f1a5908251bd3c446dce0818de8e
RedasNiuklys <redas.niuklys@ktu.edu>
2022-11-09 19:55:25 +0200

    Added Adapter

---
+++ MassHell_WPF/IConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassHell_WPF
+{
+    internal interface IConnection
+    {
+        abstract Task Connect();
+        abstract void Listen(string method,Action<dynamic> action);
+        abstract void Listen(string method, Action<dynamic, dynamic> action);
+        abstract void Listen(string method, Action<dynamic, dynamic, dynamic> action);
+
+        abstract void Send(string method);
+
+        abstract void Send(string method,object? arg1);
+        abstract void Send(string method, object? arg1,object? arg2);
+        abstract void Send(string method, object? arg1,object? arg2,object? arg3);
+
+        abstract void Create(string server);
+
+    }
+}
---
+++ MassHell_WPF/SignalRConnectionAdapter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+using MassHell_Library;
+using System.Windows;
+
+namespace MassHell_WPF
+{
+    internal class SignalRConnectionAdapter : IConnection
+    {
+        private HubConnection? adaptee;
+        Logger connectionLogger = Logger.getInstance();
+
+
+        public async Task Connect()
+        {
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection hasn't been created");
+                return;
+            }
+            await adaptee.StartAsync();
+
+        }
+
+        public void Create(string server)
+        {
+            adaptee = new HubConnectionBuilder()
+            .WithUrl(server)
+            .WithAutomaticReconnect()
+            .Build();
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection creation failed");
+            }   
+        }
+
+        public void Listen(string method,Action<dynamic> action)
+        {
+            connectionLogger.debug(action.Target.ToString());
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection hasn't been created");
+                return;
+            }
+            adaptee.On(method, action);
+        }
+        public void Listen(string method, Action<dynamic,dynamic,dynamic> action)
+        {
+            connectionLogger.debug(action.Target.ToString());
+
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection hasn't been created");
+                return;
+            }
+            adaptee.On<dynamic, dynamic, dynamic>(method, action);
+        }
+        public void Listen(string method,Action<dynamic, dynamic> action)
+        {
+            connectionLogger.debug(action.Target.ToString());
+
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection hasn't been created");
+                return;
+            }
+            adaptee.On(method, action);
+        }
+        public async void Send(string method)
+        {
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection hasn't been created");
+                return;
+            }
+            await adaptee.InvokeAsync(method);
+        }
+        public async void Send(string method, object? arg1)
+        {
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection hasn't been created");
+                return;
+            }
+            await adaptee.InvokeAsync(method, arg1);
+        }
+        public async void Send(string method, object? arg1, object? arg2)
+        {
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection hasn't been created");
+                return;
+            }
+            await adaptee.InvokeAsync(method, arg1, arg2);
+        }
+        public async void Send(string method,object? arg1, object? arg2, object? arg3)
+        {
+            if (adaptee == null)
+            {
+                connectionLogger.debug("Connection hasn't been created");
+                return;
+            }
+            await adaptee.InvokeAsync(method, arg1, arg2, arg3);
+        }
+    }
+}
