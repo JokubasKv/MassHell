0bb79c3b22a85a0508a26f0a73036d1e469d59e0
RedasNiuklys <redas.niuklys@ktu.edu>
2022-11-10 02:10:55 +0200

    Bridge bones (0 implementation)

---
+++ MassHell_WPF/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassHell_WPF
+{
+    internal class Stats : Data
+    {
+        public int kills { get; set; }
+        public int deaths   { get; set; }
+        public int powerUpsTake { get; set; }
+
+    }
+}
